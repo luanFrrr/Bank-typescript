cc78aa98413dc1b6c1d7d9679b6c72166586dc13
LuanFrrr <luanrosas@yahoo.com.br>
2023-12-13 11:32:09 -0300

    Remover node_modules do índice

---
+++ app.ts
@@ -0,0 +1,10 @@
+import { CompanyAcoount } from "./class/CompanyAccount";
+import { PeopleAccount } from "./class/PeopleAccount";
+
+//const peopleAccount: PeopleAccount = new PeopleAccount(1, "Luan", 10);
+//console.log(peopleAccount);
+//peopleAccount.deposit();
+
+const companyAccount: CompanyAcoount = new CompanyAcoount("DIO", 20);
+//companyAccount.deposit();
+console.log(companyAccount);
